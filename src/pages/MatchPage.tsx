4bc7848dd6cc5eef7b2ccc57cb5fe7be87d1a8a1
Harsh_Pachauri <harshpachauri205@gmail.com>
2025-05-29 15:34:18 +0530

    Project Done

--- src/pages/MatchPage.tsx
+++ src/pages/MatchPage.tsx
@@ -123,18 +123,18 @@ const HomePage = () => {
             leagues={leagues}
             selectedLeague={selectedLeague}
             onSelectLeague={handleSelectLeague}
-            />
+          />
         )}
       </div>
 
-<p className="text-gray-300 text-sm mt-4 font-blue">
+      <p className="text-gray-300 text-sm mt-4 font-blue">
 
-              Some leagues may not have ongoing matches right now, so no results will appear for them.
-            </p>
-            <p className=" text-gray-300 text-sm mb-2">
+        Some leagues may not have ongoing matches right now, so no results will appear for them.
+      </p>
+      <p className=" text-gray-300 text-sm mb-2">
 
-              You can try leagues like Premier League, Segunda División, Primera A, Allsvenskan, etc. for checking.
-            </p>
+        You can try leagues like Premier League, Segunda División, Primera A, Allsvenskan, etc. for checking.
+      </p>
       <div className="bg-gray-50 dark:bg-gray-800/50 rounded-lg p-6">
         <MatchesList matches={matches} isLoading={isLoadingMatches} />
       </div>
